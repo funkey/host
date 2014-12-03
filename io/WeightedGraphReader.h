5d65a4e90e3077d87954a0638de6ed1fcff5c65e
Jan Funke <funke@ini.ch>
2014-12-03 16:27:09 +0100

    bugfix: can not change graph in WeightedGraphReader while iterating over edges

--- io/WeightedGraphReader.h
+++ io/WeightedGraphReader.h
@@ -1,7 +1,6 @@
 #ifndef HOST_GRAPHS_WEIGHTED_GRAPH_READER_H__
 #define HOST_GRAPHS_WEIGHTED_GRAPH_READER_H__
 
-#include <lemon/lgf_reader.h>
 #include "WeightedGraphGenerator.h"
 
 std::istream& operator>>(std::istream& is, host::ArcType& type);
@@ -19,46 +18,7 @@ public:
 			host::Graph& graph,
 			host::ArcWeights& weights,
 			host::ArcLabels& labels,
-			host::ArcTypes& types) {
-
-		std::ifstream is(_filename.c_str());
-
-		bool isUndirected = true;
-		int  rootId = 0;
-
-		lemon::digraphReader(graph, is).
-				arcMap("weight", weights).
-				arcMap("label", labels).
-				arcMap("type", types).
-				attribute("undirected", isUndirected).
-				attribute("root", rootId).
-				run();
-
-		graph.setUndirected(isUndirected);
-		graph.setRoot(graph.nodeFromId(rootId));
-
-		if (isUndirected) {
-
-			// add arcs in opposite direction for each link arc
-			for (host::ArcIt arc(graph); arc != lemon::INVALID; ++arc) {
-
-				if (!types[arc] == host::Link)
-					continue;
-
-				addOppositeArc(graph, arc, weights, labels, types);
-			}
-
-		}
-
-		// add arcs in opposite direction for each conflict arc
-		for (host::ArcIt arc(graph); arc != lemon::INVALID; ++arc) {
-
-			if (!types[arc] == host::Conflict)
-				continue;
-
-			addOppositeArc(graph, arc, weights, labels, types);
-		}
-	}
+			host::ArcTypes& types);
 
 private:
 
@@ -67,14 +27,7 @@ private:
 			const host::Arc& arc,
 			host::ArcWeights& weights,
 			host::ArcLabels& labels,
-			host::ArcTypes& types) {
-
-		host::Arc opposite = graph.addArc(graph.target(arc), graph.source(arc));
-
-		weights[opposite] = weights[arc];
-		labels[opposite]  = labels[arc] + std::string("_opp");
-		types[opposite]   = types[arc];
-	}
+			host::ArcTypes& types);
 
 	std::string _filename;
 };
