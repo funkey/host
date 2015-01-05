a8233f0067939133c354a0b41ac1f66faeb8491a
Jan Funke <funke@ini.ch>
2015-01-05 11:25:48 +0100

    fixed computation of constant in ExclusiveTermImpl, made feasibility check robust to numerical imprecisions

--- inference/detail/ExclusiveTermImpl.h
+++ inference/detail/ExclusiveTermImpl.h
@@ -2,8 +2,10 @@
 #define HOST_INFERENCE_DETAIL_EXCLUSIVE_TERM_IMPL_H__
 
 #include <ostream>
+#include <util/helpers.hpp>
 #include <graph/Logging.h>
 #include <inference/HigherOrderArcTerm.h>
+#include <inference/Configuration.h>
 
 namespace host {
 namespace detail {
@@ -53,8 +55,39 @@ public:
 	 */
 	void addArcWeights(host::ArcWeights& weights) {
 
+		// Consider the unary costs
+		//
+		//  t(x=0) and t(x=1), where we have t(x=0)=0
+		//
+		// Our lambdas contribute the them in the following way:
+		//
+		//  t'(x=0) = t(x=0) - l(x=0)
+		//  t'(x=1) = t(x=1) - l(x=1)
+		//
+		// We want to adjust our contributions, such that t'(x=0)=0. For that, 
+		// we define
+		//
+		//  l'(x=0) = 0
+		//  l'(x=1) = l(x=1) - l(x=0),
+		//
+		// that is, we subtracted l(x=0) from our contribution. Since our 
+		// contribution is negated, that means we added a constant of l(x=0) to 
+		// the unaries:
+		//
+		//  t''(x=0) = t(x=0) - l'(x=0) = t(x=0) - l(x=0) + l(x=0)
+		//                              = t(x=0)
+		//                              nothing to do
+		//
+		//  t''(x=1) = t(x=1) - l'(x=1) = t(x=1) - l(x=1) + l(x=0)
+		//                              = t(x=1) - [l(x=1) - l(x=0)]
+		//                              = t(x=1) + [l(x=0) - l(x=1)]
+		//                              that's what we do here:
+
 		weights[_edge1] += Edge1Value0() - Edge1Value1();
 		weights[_edge2] += Edge2Value0() - Edge2Value1();
+
+		// In constant() of this term, we have to compensate for the introduced 
+		// constant l(x=0)
 	}
 
 	/**
@@ -64,8 +97,17 @@ public:
 
 		double constant = 0;
 
-		constant += _lambdas[_edge1Selected ? Edge1Value1() : Edge1Value0()];
-		constant += _lambdas[_edge2Selected ? Edge2Value1() : Edge2Value0()];
+		// The constant contribution is the minimal value obtained by optimize()
+		//
+		//   min v(e,f) = min lambda1(e) + lambda2(f)
+		//
+		// minus the constant that we introduced by setting the unaries above.
+
+		constant += (_edge1Selected ? Edge1Value1() : Edge1Value0());
+		constant += (_edge2Selected ? Edge2Value1() : Edge2Value0());
+
+		constant -= Edge1Value0();
+		constant -= Edge2Value0();
 
 		return constant;
 	}
@@ -80,8 +122,26 @@ public:
 	bool gradient(
 			const host::ArcSelection& mst,
 			Lambdas::iterator         begin,
-			Lambdas::iterator         /*end*/) {
+			Lambdas::iterator         end) {
 
+		bool feasible = (mst[_edge1] + mst[_edge2] <= 1);
+
+		// the value of this term under the current mst
+		double mstValue =
+				(mst[_edge1] ? Edge1Value1() : Edge1Value0() ) +
+				(mst[_edge1] ? Edge2Value1() : Edge2Value0() );
+
+		// if the mst is feasible (according to this term) and the solution is 
+		// very close to our min, there is no need to continue searching
+		if (feasible && std::abs(mstValue - _value) < Configuration::TermEps) {
+
+			for (Lambdas::iterator i = begin; i != end; i++)
+				*i = 0;
+
+			return true;
+		}
+
+		// the gradient is the disagreement between our term and the current mst
 		double g10 = (_edge1Selected == false) - (mst[_edge1] == false);
 		double g11 = (_edge1Selected == true ) - (mst[_edge1] == true );
 		double g20 = (_edge2Selected == false) - (mst[_edge2] == false);
@@ -93,11 +153,19 @@ public:
 		(*i) = g20; i++;
 		(*i) = g21;
 
-		return (g10*g11*g20*g21 == 0);
+		return feasible;
 	}
 
 	EdgeType edge1() const { return _edge1; }
 	EdgeType edge2() const { return _edge2; }
+
+	const Lambdas& lambdas() const { return _lambdas; }
+
+	bool operator==(const ExclusiveTermImpl<EdgeType>& other) const {
+
+		return ((_edge1 == other._edge1) && (_edge2 == other._edge2)) ||
+		       ((_edge2 == other._edge1) && (_edge1 == other._edge2));
+	}
 
 private:
 
@@ -119,11 +187,16 @@ private:
 			if (w1 < w2) {
 
 				_edge1Selected = true;
+				_value = Edge1Value1() + Edge2Value0();
 
 			} else {
 
 				_edge2Selected = true;
+				_value = Edge1Value0() + Edge2Value1();
 			}
+		} else {
+
+			_value = Edge1Value0() + Edge2Value0();
 		}
 	}
 
@@ -138,6 +211,9 @@ private:
 	// the minimal configuration of this term
 	bool _edge1Selected, _edge2Selected;
 
+	// the minimal value of this term
+	double _value;
+
 	// lambdas assicoated to this term
 	Lambdas _lambdas;
 };
@@ -148,7 +224,7 @@ private:
 template <typename EdgeType>
 std::ostream& operator<<(std::ostream& out, const host::detail::ExclusiveTermImpl<EdgeType>& term) {
 
-	out << term.edge1() << " -- " << term.edge2();
+	out << term.edge1() << " -- " << term.edge2() << "\t" << term.lambdas();
 	return out;
 }
 
