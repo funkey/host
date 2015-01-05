829f9ac8cb06e3ee1e681ad404a9e41cba516847
Jan Funke <funke@ini.ch>
2015-01-05 11:27:05 +0100

    updated multi_factors test suite

--- tests/multi_factors/multi_factors.cpp
+++ tests/multi_factors/multi_factors.cpp
@@ -6,11 +6,11 @@
 #include <inference/CandidateConflictTerm.h>
 #include <inference/MultiEdgeFactorTerm.h>
 
-void multi_tree_conflicts() {
+void multi_factors() {
 
 	boost::filesystem::path dataDir = dir_of(__FILE__);
-	boost::filesystem::path graphfile = dataDir/"tree_conflicts.dat";
-	boost::filesystem::path factorsfile = dataDir/"tree_conflicts_factors.dat";
+	boost::filesystem::path graphfile = dataDir/"small_graph.dat";
+	boost::filesystem::path factorsfile = dataDir/"small_graph_factors.dat";
 
 	host::Graph            graph;
 	host::ArcWeights       weights(graph);
@@ -20,27 +20,15 @@ void multi_tree_conflicts() {
 	host::WeightedGraphReader reader(graphfile.native());
 	reader.fill(graph, weights, labels, types);
 
-	// check if we have the conflict edges
-	for (host::ArcIt arc(graph); arc != lemon::INVALID; ++arc) {
-
-		if ((graph.id(graph.source(arc)) == 1 && (graph.id(graph.target(arc)) == 2 || graph.id(graph.target(arc)) == 3)) ||
-		    (graph.id(graph.target(arc)) == 1 && (graph.id(graph.source(arc)) == 2 || graph.id(graph.source(arc)) == 3)))
-			BOOST_CHECK_EQUAL(types[arc], host::Conflict);
-		else
-			BOOST_CHECK_EQUAL(types[arc], host::Link);
-	}
-
 	host::MultiEdgeFactors multiEdgeFactors;
 	host::MultiEdgeFactorReader factorReader(factorsfile.native());
 	factorReader.fill(graph, labels, multiEdgeFactors);
 
 	host::ExplicitWeightTerm    edgeWeightsTerm(graph, weights);
-	host::CandidateConflictTerm conflictsTerm(graph, types);
 	host::MultiEdgeFactorTerm   multiFactorsTerm(graph, multiEdgeFactors);
 
 	host::HostSearch search(graph);
 	search.addTerm(&edgeWeightsTerm);
-	search.addTerm(&conflictsTerm);
 	search.addTerm(&multiFactorsTerm);
 
 	host::ArcSelection mst(graph);
@@ -49,5 +37,5 @@ void multi_tree_conflicts() {
 
 	// check if the correct solution was found
 	BOOST_CHECK(optimal);
-	BOOST_CHECK_CLOSE(value, -0.25, 1e-6);
+	BOOST_CHECK_CLOSE(value, -3, 1e-6);
 }
--- tests/multi_factors/testsuite.cpp
+++ tests/multi_factors/testsuite.cpp
@@ -2,6 +2,7 @@
 
 BEGIN_TEST_SUITE(multi_factors)
 
+	ADD_TEST_CASE(multi_factors)
 	ADD_TEST_CASE(multi_tree_conflicts)
 
 END_TEST_SUITE()
