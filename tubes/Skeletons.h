e1076c39c774640b71440e15a4deeabc9a9dcffd
Jan Funke <funke@ini.ch>
2015-01-19 15:34:32 +0100

    made Skeletons a Volume

--- tubes/Skeletons.h
+++ tubes/Skeletons.h
@@ -1,10 +1,24 @@
 #ifndef HOST_TUBES_SKELETONS_H__
 #define HOST_TUBES_SKELETONS_H__
 
+#include <imageprocessing/Volume.h>
 #include "TubePropertyMap.h"
 #include "Skeleton.h"
 
-typedef TubePropertyMap<Skeleton> Skeletons;
+class Skeletons : public TubePropertyMap<Skeleton>, public Volume {
+
+protected:
+
+	BoundingBox computeBoundingBox() const override {
+
+		BoundingBox bb;
+
+		for (auto& p : *this)
+			bb += p.second.getBoundingBox();
+
+		return bb;
+	}
+};
 
 #endif // HOST_TUBES_SKELETONS_H__
 
