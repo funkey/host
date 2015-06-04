adc504c3180d46a32da9a1f3caa22d8cd793cad8
Jan Funke <funke@ini.ch>
2015-06-04 11:39:29 -0400

    added vtk graph export option to export_skeleton

--- binaries/export_skeleton.cpp
+++ binaries/export_skeleton.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * This program reads tubes from an HDF5 file, computes their features, and 
  * stores the result in the same file.
  */
@@ -20,6 +20,12 @@ util::ProgramOption optionTubeId(
 		util::_long_name        = "id",
 		util::_short_name       = "i",
 		util::_description_text = "The ids of the tubes of which to export skeletons. If set to 'all', all skeletons are exported.");
+
+util::ProgramOption optionFormat(
+		util::_long_name        = "format",
+		util::_short_name       = "f",
+		util::_description_text = "The format of the file to write: 'vtk' (vtk ASCII graph format), 'txt (simple text file). Default is 'txt'.",
+		util::_default_value    = "txt");
 
 int main(int argc, char** argv) {
 
@@ -54,36 +60,59 @@ int main(int argc, char** argv) {
 			}
 		}
 
+		bool vtk = (optionFormat.as<std::string>() == "vtk");
+
 		Skeletons skeletons;
 		tubeStore.retrieveSkeletons(ids, skeletons);
 
 		for (TubeId id : ids) {
 
-			std::ofstream skeletonfile(std::string("skeleton_") + boost::lexical_cast<std::string>(id) + ".txt");
+			std::ofstream skeletonfile(std::string("skeleton_") + boost::lexical_cast<std::string>(id) + (vtk ? ".vtk" : ".txt"));
+
+			if (vtk) {
+
+				skeletonfile << "# vtk DataFile Version 3.0" << std::endl;
+				skeletonfile << "vtk output" << std::endl;
+				skeletonfile << "ASCII" << std::endl;
+				skeletonfile << "DATASET UNDIRECTED_GRAPH" << std::endl;
+			}
 
 			const Skeleton& skeleton = skeletons[id];
 
 			int numNodes = 0;
+			int numEdges = 0;
 			for (Skeleton::Graph::NodeIt node(skeleton.graph()); node != lemon::INVALID; ++node)
 				numNodes++;
+			for (Skeleton::Graph::EdgeIt edge(skeleton.graph()); edge != lemon::INVALID; ++edge)
+				numEdges++;
 
 			// nodes (number of)
 
-			skeletonfile << numNodes << std::endl;
+			if (vtk)
+				skeletonfile << "POINT " << numNodes << " float" << std::endl;
+			else
+				skeletonfile << numNodes << std::endl;
 
 			// node positions (node x y z)
-			for (Skeleton::Graph::NodeIt node(skeleton.graph()); node != lemon::INVALID; ++node)
+			for (int nodeId = 0; nodeId < numNodes; nodeId++) {
+
+				Skeleton::Graph::Node node = skeleton.graph().nodeFromId(nodeId);
+
+				if (!vtk)
+					skeletonfile
+							<< skeleton.graph().id(node) << "\t";
+
 				skeletonfile
-						<< skeleton.graph().id(node) << "\t"
 						<< skeleton.positions()[node].x() << "\t"
 						<< skeleton.positions()[node].y() << "\t"
 						<< skeleton.positions()[node].z() << std::endl;
+			}
 
-			// node diameters (node d)
-			for (Skeleton::Graph::NodeIt node(skeleton.graph()); node != lemon::INVALID; ++node)
-				skeletonfile
-						<< skeleton.graph().id(node) << "\t"
-						<< skeleton.diameters()[node] << std::endl;
+			if (vtk) {
+
+				skeletonfile << "VERTICES " << numNodes << std::endl;
+				skeletonfile << "EDGES " << numEdges << std::endl;
+			}
 
 			// edges (pairs of nodes)
 
@@ -91,6 +120,22 @@ int main(int argc, char** argv) {
 				skeletonfile
 						<< skeleton.graph().id(skeleton.graph().u(edge)) << "\t"
 						<< skeleton.graph().id(skeleton.graph().v(edge)) << std::endl;
+
+			// node diameters (node d)
+
+			if (vtk) {
+
+				skeletonfile << "VERTEX_DATA " << numNodes << std::endl;
+				skeletonfile << "FIELD FieldData 1" << std::endl;
+				skeletonfile << "diameters 1 " << numNodes << " double" << std::endl;
+			}
+			for (int nodeId = 0; nodeId < numNodes; nodeId++) {
+
+				Skeleton::Graph::Node node = skeleton.graph().nodeFromId(nodeId);
+				skeletonfile
+						<< skeleton.graph().id(node) << "\t"
+						<< skeleton.diameters()[node] << std::endl;
+			}
 		}
 
 	} catch (boost::exception& e) {
